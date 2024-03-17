65604dc9c873df08a3b6949097d7e5b1951afbb4
owilo <noye.valentin@gmail.com>
2024-03-17 17:25:51 +0100

    FH Update

--- Code/cpp/felzenszwalb.cpp
+++ Code/cpp/felzenszwalb.cpp
@@ -22,7 +22,56 @@ struct Component {
     colorm::Lab color;
 };
 
-void felzenszwalb(std::uint8_t* inputImage, std::uint8_t* outputImage, int width, int height, double c) {
+double getComponents(Component*& firstComponent, Component*& secondComponent, Component** componentMap, colorm::Lab* pixelColors, int* horizontalEdges, int& iHorizontal, int horizontalEdgesCount, int* verticalEdges, int& iVertical, int verticalEdgesCount, int width) {
+    int firstPixel;
+    int secondPixel;
+    bool checkHorizontal{iHorizontal < horizontalEdgesCount};
+    bool checkVertical{iVertical < verticalEdgesCount};
+    if (checkHorizontal && checkVertical) {
+        double edgeWeightHorizontal{pixelColors[horizontalEdges[iHorizontal]].distance(pixelColors[horizontalEdges[iHorizontal] + 1])};
+        double edgeWeightVertical{pixelColors[verticalEdges[iVertical]].distance(pixelColors[verticalEdges[iVertical] + width])};
+        checkHorizontal = edgeWeightHorizontal <= edgeWeightVertical;
+    }
+
+    if (checkHorizontal) {
+        firstPixel = horizontalEdges[iHorizontal];
+        secondPixel = firstPixel + 1;
+        ++iHorizontal;
+    }
+    else {
+        firstPixel = verticalEdges[iVertical];
+        secondPixel = firstPixel + width;
+        ++iVertical;
+    }
+
+    firstComponent = componentMap[firstPixel];
+    secondComponent = componentMap[secondPixel];
+
+    return firstComponent != secondComponent ? pixelColors[firstPixel].distance(pixelColors[secondPixel]) : 0.0;
+}
+
+void merge(Component** componentMap, Component* firstComponent, Component* secondComponent, double edgeWeight) {
+    std::size_t firstComponentPixelCount{firstComponent->pixels.size()};
+    std::size_t secondComponentPixelCount{firstComponent->pixels.size()};
+
+    // Le poids de l'arête devient le poids maximal du MST de cette fusion (car il connecte les deux parties du graphe dont les arêtes ont un poids inférieur)
+    firstComponent->internalMaxMST = edgeWeight;
+
+    // Calcul de la nouvelle moyenne des pixels de la fusion
+    double denom{1.0 / (firstComponentPixelCount + secondComponentPixelCount)};
+
+    firstComponent->color.setLightness(denom * (firstComponent->color.lightness() * firstComponentPixelCount + secondComponent->color.lightness() * secondComponentPixelCount));
+    firstComponent->color.setA(denom * (firstComponent->color.a() * firstComponentPixelCount + secondComponent->color.a() * secondComponentPixelCount));
+    firstComponent->color.setB(denom * (firstComponent->color.b() * firstComponentPixelCount + secondComponent->color.b() * secondComponentPixelCount));
+
+    // Fusion des components
+    for (int pixel : secondComponent->pixels) {
+        componentMap[pixel] = firstComponent;
+    }
+    firstComponent->pixels.insert(firstComponent->pixels.end(), secondComponent->pixels.begin(), secondComponent->pixels.end());
+}
+
+void felzenszwalb(std::uint8_t* inputImage, std::uint8_t* outputImage, int width, int height, double c, int minSize) {
     c /= 255.0;
 
     // Initialisation
@@ -48,7 +97,7 @@ void felzenszwalb(std::uint8_t* inputImage, std::uint8_t* outputImage, int width
 
     // Construction des arêtes horizontales
     int horizontalEdgesCount{(width - 1) * height};
-    std::vector<double> horizontalEdges(horizontalEdgesCount);
+    std::vector<int> horizontalEdges(horizontalEdgesCount);
     int iHorizontal{};
     for (int i{}; i < width - 1; ++i) {
         for (int j{}; j < height; ++j) {
@@ -63,7 +112,7 @@ void felzenszwalb(std::uint8_t* inputImage, std::uint8_t* outputImage, int width
 
     // Construction des arêtes verticales
     int verticalEdgesCount{width * (height - 1)};
-    std::vector<double> verticalEdges(verticalEdgesCount);
+    std::vector<int> verticalEdges(verticalEdgesCount);
     int iVertical{};
     for (int i{}; i < width; ++i) {
         for (int j{}; j < height - 1; ++j) {
@@ -82,60 +131,38 @@ void felzenszwalb(std::uint8_t* inputImage, std::uint8_t* outputImage, int width
     iHorizontal = 0;
     iVertical = 0;
     for (int i{}; i < horizontalEdgesCount + verticalEdgesCount; ++i) {
-        int firstPixel;
-        int secondPixel;
-        bool checkHorizontal{iHorizontal < horizontalEdgesCount};
-        bool checkVertical{iVertical < verticalEdgesCount};
-        if (checkHorizontal && checkVertical) {
-            double edgeWeightHorizontal{pixelColors[horizontalEdges[iHorizontal]].distance(pixelColors[horizontalEdges[iHorizontal] + 1])};
-            double edgeWeightVertical{pixelColors[verticalEdges[iVertical]].distance(pixelColors[verticalEdges[iVertical] + width])};
-            checkHorizontal = edgeWeightHorizontal <= edgeWeightVertical;
-        }
-
-        if (checkHorizontal) {
-            firstPixel = horizontalEdges[iHorizontal];
-            secondPixel = firstPixel + 1;
-            ++iHorizontal;
-        }
-        else {
-            firstPixel = verticalEdges[iVertical];
-            secondPixel = firstPixel + width;
-            ++iVertical;
-        }
-
-        Component* firstComponent{componentMap[firstPixel]};
-        Component* secondComponent{componentMap[secondPixel]};
+        Component* firstComponent;
+        Component* secondComponent;
+        double edgeWeight{getComponents(firstComponent, secondComponent, componentMap.data(), pixelColors.data(), horizontalEdges.data(), iHorizontal, horizontalEdgesCount, verticalEdges.data(), iVertical, verticalEdgesCount, width)};
 
         // Les pixels sont déjà dans le même component, on ne fait rien
         if (firstComponent == secondComponent) {
             continue;
         }
 
-        double edgeWeight{pixelColors[firstPixel].distance(pixelColors[secondPixel])};
-
         // Si l'équation satisfait le prédicat de fusion
-        std::size_t firstComponentPixelCount{firstComponent->pixels.size()};
-        std::size_t secondComponentPixelCount{firstComponent->pixels.size()};
-        if (edgeWeight <= std::min(firstComponent->internalMaxMST + c / firstComponentPixelCount, secondComponent->internalMaxMST + c / secondComponentPixelCount)) {
-
-            // Le poids de l'arête devient le poids maximal du MST de cette fusion (car il connecte les deux parties du graphe dont les arêtes ont un poids inférieur)
-            firstComponent->internalMaxMST = edgeWeight;
-
-            // Calcul de la nouvelle moyenne des pixels de la fusion
-            double denom{1.0 / (firstComponentPixelCount + secondComponentPixelCount)};
-
-            firstComponent->color.setLightness(denom * (firstComponent->color.lightness() * firstComponentPixelCount + secondComponent->color.lightness() * secondComponentPixelCount));
-            firstComponent->color.setA(denom * (firstComponent->color.a() * firstComponentPixelCount + secondComponent->color.a() * secondComponentPixelCount));
-            firstComponent->color.setB(denom * (firstComponent->color.b() * firstComponentPixelCount + secondComponent->color.b() * secondComponentPixelCount));
-
-            // Fusion des components
-            for (int pixel : secondComponent->pixels) {
-                componentMap[pixel] = firstComponent;
-            }
-            firstComponent->pixels.insert(firstComponent->pixels.end(), secondComponent->pixels.begin(), secondComponent->pixels.end());
-
+        if (edgeWeight <= std::min(firstComponent->internalMaxMST + c / firstComponent->pixels.size(), secondComponent->internalMaxMST + c / secondComponent->pixels.size())) {
+            merge(componentMap.data(), firstComponent, secondComponent, edgeWeight);
             --componentsCount;
+            delete secondComponent;
+        }
+    }
 
+    // Fusion des éléments de petite taille 
+    iHorizontal = 0;
+    iVertical = 0;
+    for (int i{}; i < horizontalEdgesCount + verticalEdgesCount; ++i) {
+        Component* firstComponent;
+        Component* secondComponent;
+        double edgeWeight{getComponents(firstComponent, secondComponent, componentMap.data(), pixelColors.data(), horizontalEdges.data(), iHorizontal, horizontalEdgesCount, verticalEdges.data(), iVertical, verticalEdgesCount, width)};
+
+        if (firstComponent == secondComponent) {
+            continue;
+        }
+
+        if (firstComponent->pixels.size() < minSize || secondComponent->pixels.size() < minSize) {
+            merge(componentMap.data(), firstComponent, secondComponent, edgeWeight);
+            --componentsCount;
             delete secondComponent;
         }
     }
@@ -151,8 +178,8 @@ void felzenszwalb(std::uint8_t* inputImage, std::uint8_t* outputImage, int width
 }
 
 int main(int argc, const char** argv) {
-    if (argc < 4) {
-        std::cout << "Usage :\n  - Image d'entrée\n  - Image de sortie\n  - Seuil k (300.0 par exemple)\n";
+    if (argc < 5) {
+        std::cout << "Usage :\n  - Image d'entrée\n  - Image de sortie\n  - Seuil k (1000.0 par exemple)\n  - Taille minimale d'un segment t (30 par exemple)\n";
         return 0;
     }
 
@@ -162,7 +189,7 @@ int main(int argc, const char** argv) {
     std::uint8_t* inputImage{stbi_load(argv[1], &width, &height, &channelsCount, STBI_rgb)};
     std::uint8_t* outputImage = new std::uint8_t[3 * width * height];
     
-    felzenszwalb(inputImage, outputImage, width, height, std::atof(argv[3]));
+    felzenszwalb(inputImage, outputImage, width, height, std::atof(argv[3]), std::atoi(argv[4]));
 
     stbi_write_png(argv[2], width, height, STBI_rgb, outputImage, 3 * width);
 
